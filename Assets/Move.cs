31f343b7cc8d5cbb9db8d8c64ca4513fd2e13d1c
JuniosSTL <junios@naver.com>
2017-09-16 16:24:55 +0900

    사용자 컴포넌트 만들어 보기 이동

---
+++ Assets/Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Transform myTransform;
+    AudioSource source;
+
+	void Start ()
+	{
+        //시작 하자 마자 GameObject에서 Transform 컴포넌트를 찾아서
+        //myTransform에 할당
+        myTransform = GetComponent<Transform>();
+        source = GetComponent<AudioSource>();
+    }
+	
+	void Update ()
+	{
+        //myTransform.Translate(0, 1 * Time.deltaTime, 0);
+        transform.Translate(0, 1 * Time.deltaTime, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            source.Play();
+        }
+    }
+}
